68608122b58e9fae08acdfc86f9aa80dd88b829c
Samanta García <38795142+SamantaGarcia@users.noreply.github.com>
2019-10-11 06:29:32 -0500

    Inventario con listas enlazadas

---
+++ act16_ListasEnlazadas/Lists.js
@@ -0,0 +1,86 @@
+export default class List{
+    constructor(schedule){
+        this._schedule = schedule;
+        this._list = '';
+        this._init = null;
+        this._temp = null;
+    }
+
+    get init(){
+        return this._init;
+    }
+    set init(init){
+        this._init = init;
+    }
+    get temp(){
+        return this._temp;
+    }
+    set temp(temp){
+        this._temp = temp;
+    }
+
+    _addObject(product){
+        let objProduct = {
+            code : product.code,
+            name : product.name,
+            price : product.price,
+            quantity : product.quantity,
+            description : product.description
+        };
+        this._show(objProduct); 
+    }
+
+    add(newP){
+        if (this._init == null) {
+            this._init = newP;
+        }else {
+            this._addNext(newP, this._init);
+        }
+        this._addObject(newP);
+        
+    }
+
+    _addNext(newP, last){
+        if (last._next == null) {
+                last._next = newP;
+        } else {
+            this._addNext(newP, last._next);
+                console.log(last._next);
+        }
+             
+    }
+
+    _show(newP){
+        this._list+= 'code: ' + newP.code +
+                ' name: ' + newP.name + 
+                ' price: ' + newP.price + 
+                ' quantity: ' + newP.quantity + 
+                ' description: ' + newP.description + '\n';
+            document.getElementById('div').innerText = this._list;
+    }
+
+    Search(value){
+        this._temp = this._init;
+
+        while(this._temp.code != value){
+            this._temp=this._temp._next;
+        }              
+            console.log(this._temp);
+            this._list='';
+            this._show(this._temp);
+            
+        
+    }
+    Delete(value){
+        this._temp = this._init;
+
+        while(this._temp._next.code != value){
+            this._temp=this._temp._next;
+        }              
+        if (this._temp.code == value) {
+            this._temp._next._next == this._temp._next;
+        }
+            console.error("delete: "+this._temp._next);
+    }
+
+}
---
+++ act16_ListasEnlazadas/main.js
@@ -0,0 +1,42 @@
+import List from "./Lists.js"
+import Product from "./List.js"
+
+class Main{
+    constructor(){
+        let schedule = new List(document.querySelector("#div"));
+    
+
+    document.querySelector("#btnAdd").addEventListener("click", () =>{
+        let form = document.querySelector("#form");
+
+        if (form.checkValidity() === true) {
+            let code = document.querySelector("#Code").value;
+            let name = document.querySelector("#Name").value;
+            let price = document.querySelector("#Price").value;
+            let quantity = document.querySelector("#Quantity").value;
+            let description = document.querySelector("#Description").value;
+            let objProduct = {
+                code : code,
+                name : name,
+                price : price,
+                quantity : quantity,
+                description : description
+            };
+
+            let product = new Product(objProduct);
+            schedule.add(product);
+        } form.classList.add("was-validated");
+    });
+
+    document.querySelector("#btnSearch").addEventListener("click", () =>{
+        let searchName = document.querySelector("#searchName").value;
+        schedule.Search(searchName);
+    });
+
+    document.querySelector("#btnDelete").addEventListener("click", () =>{
+        let deleteName = document.querySelector("#searchName").value;
+        schedule.Delete(deleteName);
+    });
+};
+}
+let m = new Main();
